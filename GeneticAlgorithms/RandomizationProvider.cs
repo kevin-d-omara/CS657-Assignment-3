23e91008865f4eea7a6e30776590027db1541dd5
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-03-27 11:26:29 -0700

    Add a global point of access to random numbers

---
+++ GeneticAlgorithms/RandomizationProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KevinDOMara.SDSU.CS657.Assignment3.GeneticAlgorithms
+{
+    public static class RandomizationProvider
+    {
+        public static Random random = new Random();
+    }
+}
