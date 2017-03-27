27c377078de2ce3b457e1c5c39637d262b82d6e6
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-03-27 11:27:05 -0700

    Add class GeneticOperators

---
+++ GeneticAlgorithms.UnitTests/GeneticOperatorsTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace KevinDOMara.SDSU.CS657.Assignment3.GeneticAlgorithms.UnitTests
+{
+    [TestFixture]
+    public class GeneticOperatorsTest
+    {
+        RouteChromosome parentA;
+        RouteChromosome parentB;
+        Point homeA;
+        Point homeB;
+        Point[] genesA;
+        Point[] genesB;
+
+        [SetUp]
+        public void SetUp()
+        {
+            homeA = new Point(0, 0);
+            homeB = new Point(2, 0);
+            genesA = new Point[]
+            {
+                new Point(1, 0),
+                new Point(1, 1),
+                new Point(0, 1)
+            };
+            genesB = new Point[]
+            {
+                new Point(4, 0),
+                new Point(4, 2),
+                new Point(2, 2)
+            };
+
+            parentA = new RouteChromosome(homeA, genesA);
+            parentB = new RouteChromosome(homeB, genesB);
+        }
+
+        [Test]
+        public void Clone()
+        {
+            var cloneA = GeneticOperators.Clone(parentA);
+
+            // Modify original chromosome, but clone is untouched.
+            var genesA2 = (Point[])genesA.Clone();
+            genesA2[0] = new Point(7, 7);
+            parentA = new RouteChromosome(homeA, genesA2);
+
+            Assert.AreNotEqual(parentA.Genes[0], cloneA.Genes[0]);
+            Assert.AreNotEqual(parentA.Fitness, cloneA.Fitness);
+        }
+
+        [Test]
+        public void Mutate()
+        {
+            // TODO
+        }
+    }
+}
---
+++ GeneticAlgorithms/GeneticOperators.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KevinDOMara.SDSU.CS657.Assignment3.GeneticAlgorithms
+{
+    /// <summary>
+    /// Operations that can be used to create modified chromosomes.
+    /// </summary>
+    public class GeneticOperators
+    {
+        /// <summary>
+        /// Return a shallow copy of the chromosome.
+        /// </summary>
+        public static RouteChromosome Clone(RouteChromosome chromosome)
+        {
+            var genes = (Point[])chromosome.Genes.Clone();
+            var clone = new RouteChromosome(chromosome.Home, genes);
+
+            return clone;
+        }
+
+        /// <summary>
+        /// Return a shallow copy of the chromosome with the position of two genes swapped.
+        /// </summary>
+        public static RouteChromosome Mutate(RouteChromosome chromosome)
+        {
+            var genes = (Point[])chromosome.Genes.Clone();
+
+            var index1 = RandomizationProvider.random.Next(chromosome.Length);
+            var index2 = RandomizationProvider.random.Next(chromosome.Length);
+            var gene1 = genes[index1];
+            var gene2 = genes[index2];
+
+            genes[index1] = gene2;
+            genes[index2] = gene1;
+
+            var mutant = new RouteChromosome(chromosome.Home, genes);
+
+            return mutant;
+        }
+    }
+}
