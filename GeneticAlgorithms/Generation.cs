5f2928b3f8e148057d072fe0b4cb5b3e6a5a078b
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-03-28 08:03:05 -0700

    Flesh out interface for Generation and Population

--- GeneticAlgorithms/Generation.cs
+++ GeneticAlgorithms/Generation.cs
@@ -7,12 +7,12 @@ namespace KevinDOMara.SDSU.CS657.Assignment3.GeneticAlgorithms
     /// <summary>
     /// A set of chromosomes.
     /// </summary>
-    public class Generation<T> where T : IChromosome
+    public class Generation
     {
         /// <summary>
         /// The list of chromosomes.
         /// </summary>
-        public T[] Chromosomes { get; private set; }
+        public IChromosome[] Chromosomes { get; private set; }
 
         /// <summary>
         /// Number of chromosomes in this generation.
@@ -20,9 +20,14 @@ namespace KevinDOMara.SDSU.CS657.Assignment3.GeneticAlgorithms
         public int Number { get; private set; }
 
         /// <summary>
+        /// The most fit chromosome (i.e. the candidate solution).
+        /// </summary>
+        public IChromosome FittestChromosome { get; private set; }
+
+        /// <summary>
         /// Create a generation from the given set of chromosomes.
         /// </summary>
-        public Generation(List<T> chromosomes)
+        public Generation(List<IChromosome> chromosomes)
         {
             Number = chromosomes.Count;
             Chromosomes = chromosomes.ToArray();
--- GeneticAlgorithms/Population.cs
+++ GeneticAlgorithms/Population.cs
@@ -7,17 +7,17 @@ namespace KevinDOMara.SDSU.CS657.Assignment3.GeneticAlgorithms
     /// <summary>
     /// A set of generations.
     /// </summary>
-    public class Population<T> where T : IChromosome
+    public class Population
     {
         /// <summary>
         /// The set of generations.
         /// </summary>
-        public Generation<T>[] Generations { get; private set; }
+        public List<Generation> Generations { get; private set; }
 
         /// <summary>
         /// Youngest generation.
         /// </summary>
-        public Generation<T> LatestGeneration { get; private set; }
+        public Generation LatestGeneration { get; private set; }
 
         /// <summary>
         /// Number of generations.
@@ -25,13 +25,32 @@ namespace KevinDOMara.SDSU.CS657.Assignment3.GeneticAlgorithms
         public int GenerationNumber { get; private set; }
 
         /// <summary>
-        /// Create a new population.
+        /// Number of chromosomes in each generation.
+        /// </summary>
+        public int Size { get; private set; }
+
+        /// <summary>
+        /// The number of genes in each chromosome.
+        /// </summary>
+        public int ChromosomeLength { get; private set; }
+
+        /// <summary>
+        /// Create a new population with a randomized first generation.
         /// </summary>
         /// <param name="size">The number of chromosomes in each generation.</param>
         /// <param name="chromosomeLength">The length of each chromosome.</param>
         public Population(int size, int chromosomeLength)
         {
 
+        }
+
+        /// <summary>
+        /// Create the next generation by using selection on the latest generation.
+        /// </summary>
+        public void CreateNextGeneration()
+        {
+            // Use Selection to choose Size/2 pairs.
+            // Use GeneticOperators to create children.
         }
     }
 }
