0e6cecb8fc6babae179c072d0c1313b6765fa985
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-03-29 11:45:56 -0700

    Remove need for GeneticAlgos. to call constructor
    Currently, GeneticAlgorithms must call the chromosome's constructor.
    This disallows chromosomes from having a custom, multi-parameter
    constructor.
    
    These new IChromosome methods allow GeneticAlgorithms to make new
    chromosomes and make modifications without calling a constructor.
    
    Ultimately, Population will be the only place a chromosome's
    constructor will be called. This is acceptable because Population
    must be modified on a per-problem / per-chromosome-type basis.

--- GeneticAlgorithms/IChromosome.cs
+++ GeneticAlgorithms/IChromosome.cs
@@ -5,7 +5,8 @@ using System.Linq;
 namespace KevinDOMara.SDSU.CS657.Assignment3.GeneticAlgorithms
 {
     /// <summary>
-    /// Interface for a generic immutable chromosome.
+    /// Interface for a generic chromosome. Any changes to the genes must be made through the
+    /// appropriate getter and setter methods.
     /// </summary>
     public interface IChromosome
     {
@@ -18,5 +19,37 @@ namespace KevinDOMara.SDSU.CS657.Assignment3.GeneticAlgorithms
         /// Gets the fitness according to some metric.
         /// </summary>
         float Fitness { get; }
+
+        /// <summary>
+        /// Returns a shallow copy of the chromosome.
+        /// </summary>
+        IChromosome Clone();
+
+        /// <summary>
+        /// Returns the gene at the index.
+        /// </summary>
+        Gene GetGene(int index);
+
+        /// <summary>
+        ///  Returns the genes between the start and end indices.
+        /// </summary>
+        Gene[] GetGenes(int startIndex, int endIndex);
+
+        /// <summary>
+        /// Returns the entire gene array.
+        /// </summary>
+        Gene[] GetGenes();
+
+        /// <summary>
+        /// Replaces the gene at the index.
+        /// </summary>
+        void ReplaceGene(int index, Gene gene);
+
+        /// <summary>
+        /// Replaces the genes starting at index. If there are more genes provided than there are
+        /// genes to replace, the excess genes will be appended to the chromosome (i.e. the
+        /// chromosome's length will be increased).
+        /// </summary>
+        void ReplaceGenes(int index, Gene[] genes);
     }
 }
